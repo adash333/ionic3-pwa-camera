2c1d46f08a51c5916e30bc21c073c8e564754ef3
adash333 <adash333@hotmail.com>
2018-10-10 18:53:13 +0900

    home.html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,8 +7,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('pwaphoto') pwaphoto: ElementRef;
+
+  imgURI: string = null;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  openPWAPhotoPicker() {
+    if (this.pwaphoto == null) {
+      return;
+    }
+
+    this.pwaphoto.nativeElement.click();
+  }
+
+  uploadPWA() {
+    if (this.pwaphoto == null ) {
+      return;
+    }
+
+    const fileList: FileList = this.pwaphoto.nativeElement.files;
+
+    if (fileList && fileList.length > 0) {
+      this.firstFileToBase64(fileList[0]).then((result: string) => {
+        this.imgURI = result;
+      }, (err: any) => {
+        // Ignore error, do nothing
+        this.imgURI = null;
+      });
+    }
+  }
+
+  private firstFileToBase64(fileImage: File): Promise<{}> {
+    return new Promise((resolve, reject) => {
+      let fileReader: FileReader = new FileReader();
+      if (fileReader && fileImage != null) {
+        fileReader.readAsDataURL(fileImage);
+        fileReader.onload = () => {
+          resolve(fileReader.result);
+        };
+
+        fileReader.onerror = (error) => {
+          reject(error);
+        };
+      } else {
+        reject(new Error('No file found'));
+      }
+    });
   }
 
 }
